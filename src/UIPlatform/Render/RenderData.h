7c6b1d0a21dfdc83e657da3a284235bfaee891a1
russo-2025 <russo-2025@mail.ru>
2025-10-01 15:23:27 +0300

    Revert "Remove commented legacy members from RenderData"
    This reverts commit 3db86f22e99a97e6346261396e99d0511731bb08.

--- src/UIPlatform/Render/RenderData.h
+++ src/UIPlatform/Render/RenderData.h
@@ -48,7 +48,17 @@ namespace NL::Render
     };
 
     struct RenderData
-    {
+    {/*
+        ID3D11Device* device = nullptr;
+        ID3D11DeviceContext3* deviceContext = nullptr;
+        Microsoft::WRL::ComPtr<ID3D11DeviceContext> deferredContext;
+        std::unique_ptr<DirectX::SpriteBatch> spriteBatchDeferred;
+        std::shared_ptr<DirectX::CommonStates> commonStates = nullptr;
+        ID3D11ShaderResourceView* texture = nullptr; 
+        std::uint32_t width = 0;
+        std::uint32_t height = 0;
+        Common::SpinLock drawLock;
+        */
         Microsoft::WRL::ComPtr<ID3D11Device> device;
         Microsoft::WRL::ComPtr<ID3D11Device1> device1;
         Microsoft::WRL::ComPtr<ID3D11DeviceContext3> immCtx;
